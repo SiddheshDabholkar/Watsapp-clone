f96caf9c0e3de10dccac875bd81d6f98fd413f94
siddhesh <iamsiddhesh22@gmail.com>
2021-10-31 01:20:07 +0530

    profile photo screen complete

--- screens/ProfilePhoto.jsx
+++ screens/ProfilePhoto.jsx
@@ -1,13 +1,62 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Box } from 'native-base';
+import React, { useLayoutEffect } from 'react';
+import { TouchableOpacity, Pressable, StyleSheet, Dimensions, Share } from 'react-native';
+import { Box, HStack, Image, Button } from 'native-base';
+import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function ProfilePhoto() {
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
+export default function ProfilePhoto({ navigation }) {
+
+    const onShare = async () => {
+        try {
+            const result = await Share.share({
+                message: "Its my dp",
+            });
+            if (result.action === Share.sharedAction) {
+                if (result.activityType) {
+                    // shared with activity type of result.activityType
+                } else {
+                    // shared
+                }
+            } else if (result.action === Share.dismissedAction) {
+                // dismissed
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <HStack space={3}>
+                    <TouchableOpacity>
+                        <MaterialCommunityIcons name="pencil" size={24} color="black" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={onShare}>
+                        <Entypo name="share" size={24} color="black" />
+                    </TouchableOpacity>
+                </HStack>
+            )
+        });
+    }, [navigation]);
     return (
-        <Box flex={1} bg="#111827">
-            <View>
-                <Text></Text>
-            </View>
-        </Box>
+        <Box flex={1} bg="#000" alignItems="center" justifyContent="center" >
+            <Image
+                alt="Alternate Text"
+                source={{
+                    uri: 'https://reactnative.dev/img/tiny_logo.png',
+                }}
+                style={styles.image}
+            />
+        </Box >
     );
 }
+
+const styles = StyleSheet.create({
+    image: {
+        height: height / 2.2,
+        width: width,
+    }
+});
