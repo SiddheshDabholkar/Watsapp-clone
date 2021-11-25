a5c3c1b630db4a80228a04f108c7c2d177d79907
siddhesh <iamsiddhesh22@gmail.com>
2021-11-26 03:29:09 +0530

    message component refactored

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, HStack, VStack, Text } from "native-base";
+import { Box, Text, HStack } from "native-base";
 
 export default function Message({ item }) {
   const me = item.userId === 1;
@@ -8,15 +8,44 @@ export default function Message({ item }) {
       w="100%"
       alignItems={`flex-${me ? "end" : "start"}`}
       justifyContent={`flex-${me ? "end" : "start"}`}
-      mt="2"
+      mt="3"
     >
-      <Box w="70%" bg={!me ? "#262d31" : "#056162"} borderRadius="5">
-        {/* <Box style={{ alignSelf: "flex-start" }}> */}
-        <Text color="#fff" p="2">
-          {item?.message}
-        </Text>
-        {/* </Box> */}
-      </Box>
+      <HStack w="70%">
+        <HStack
+          w="100%"
+          flex={1}
+          justifyContent={`flex-${me ? "end" : "start"}`}
+        >
+          <Box
+            style={{
+              alignSelf: `flex-${me ? "end" : "start"}`,
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20,
+              alignItems: "center",
+              maxWidth: "75%",
+            }}
+            bg={!me ? "#262d31" : "#056162"}
+          >
+            <Text color="#fff" p="2">
+              {item?.message}
+            </Text>
+          </Box>
+          <Box
+            alignItems="flex-end"
+            justifyContent="flex-end"
+            bg={!me ? "#262d31" : "#056162"}
+            style={{
+              borderTopRightRadius: 20,
+              borderBottomRightRadius: 20,
+              maxWidth: "25%",
+            }}
+          >
+            <Text color="#fff" fontSize="10" ml="2" p="2">
+              {item?.time}
+            </Text>
+          </Box>
+        </HStack>
+      </HStack>
     </Box>
   );
 }
